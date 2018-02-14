8a0deb9031be59e35f668b4241f9d8d74496590f
hieusydo <hieusydo@gmail.com>
2018-02-14 09:44:52 -0500

    Set up dir structure for server and static files

---
+++ static/js/index.jsx
@@ -0,0 +1,10 @@
+// index.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = <h1>Bon Voyage!</h1>;
+
+ReactDOM.render(
+    title,
+    document.getElementById("map")
+);
